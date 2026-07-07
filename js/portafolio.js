a8ebe8419c48ce20b875542c80ae3e3d276abc74
JaredTareas <jaredgarciagarcia0404@gmail.com>
2026-07-06 23:47:52 -0600

    nuevo

--- js/portafolio.js
+++ js/portafolio.js
@@ -1,4 +1,28 @@
-document.getElementById("btnSaludo").addEventListener("click", function() {
+const textos = [
+    "Estudiante de Ingeniería en Sistemas Computacionales",
+    "Estudiante del Instituto Tecnológico de Oaxaca",
+    "Desarrollador Web en formación",
+    "Aprendiendo HTML, CSS, JavaScript y Bootstrap"
+];
+
+let indice = 0;
+const textoCambiante = document.getElementById("textoCambiante");
+
+setInterval(function () {
+    indice++;
+
+    if (indice >= textos.length) {
+        indice = 0;
+    }
+
+    textoCambiante.innerHTML = textos[indice];
+}, 2500);
+
+document.getElementById("btnSaludo").addEventListener("click", function () {
     document.getElementById("mensaje").innerHTML =
-        "Holaaaa, Bienvenido a mi portafolio personal.";
+        "¡Hola! Bienvenido a mi portafolio personal.";
+});
+
+document.getElementById("btnMenu").addEventListener("click", function () {
+    document.getElementById("header").classList.toggle("mostrar");
 });
