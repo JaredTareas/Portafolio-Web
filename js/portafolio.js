d8a3dacc0bf9d04133965002f4853924d2d67702
JaredTareas <jaredgarciagarcia0404@gmail.com>
2026-07-06 20:17:22 -0600

    Primer commit del portafolio

---
+++ js/portafolio.js
@@ -0,0 +1,4 @@
+document.getElementById("btnSaludo").addEventListener("click", function() {
+    document.getElementById("mensaje").innerHTML =
+        "Holaaaa, Bienvenido a mi portafolio personal.";
+});
